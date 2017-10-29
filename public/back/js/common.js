cf88605b8d1bbdc5d95046d4262174fcc4190eb8
cd <905702879@qq.com>
2017-10-29 21:49:45 +0800

    首页面完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,54 @@
 /**
  * Created by 程丹 on 2017/10/29.
  */
+if(location.href.indexOf("login.html") < 0){
+    $.ajax({
+        type: "get",
+        url : "/employee/checkRootLogin",
+        success : function (data) {
+            if(data.error === 400){
+                location.href = "login.html";
+            }
+        }
+    });
+};
+
+$(document).ajaxStart(function () {
+    NProgress.start();
+});
+$(document).ajaxStop(function () {
+    setTimeout(function () {
+        //让进度条结束
+        NProgress.done();
+    }, 500);
+});
+
+$(function () {
+    $(".child").prev().on("click",function () {
+        // console.log(1);
+        $(this).next().slideToggle();
+    });
+    
+    $(".icon_menu").on("click",function () {
+        $(".lt_aside").toggleClass("now");
+        $(".lt_main").toggleClass("now");
+    });
+    
+    $(".icon_logout").on("click",function () {
+        // console.log(1);
+        $("#logoutModal").modal("show");
+    });
+
+    $(".btn_logout").on("click",function () {
+        $.ajax({
+            type : "get",
+            url : "/employee/employeeLogout",
+            success : function (data) {
+                if(data.success){
+                    location.href = "login.html";
+                }
+            }
+        })
+    })
+});
+
