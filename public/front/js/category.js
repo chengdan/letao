6dcaa4c4050211bcb64216bde750591831ab26f0
cd <905702879@qq.com>
2017-11-01 21:58:58 +0800

    前端页面部分完成

---
+++ public/front/js/category.js
@@ -0,0 +1,35 @@
+/**
+ * Created by 程丹 on 2017/11/1.
+ */
+mui('.mui-scroll-wrapper').scroll({
+    indicators: false
+});
+
+$.ajax({
+    type:"get",
+    url:"/category/queryTopCategory",
+    success:function (data) {
+        $(".lt_aside ul").html( template("tpl",data));
+        renderSecond(data.rows[0].id);
+    }
+});
+
+function renderSecond(id) {
+    $.ajax({
+        type:"get",
+        url:"/category/querySecondCategory",
+        data : {
+            id : id
+        },
+        success: function (data) {
+            $(".lt_main ul").html(template("tpl2",data))
+        }
+    })
+}
+
+$(".lt_aside ul").on("click","li",function () {
+    $(this).addClass("now").siblings().removeClass("now");
+    var id = $(this).data("id");
+    renderSecond(id);
+    // console.log(id);
+});
---
+++ public/front/js/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by 程丹 on 2017/11/1.
+ */
+mui('.mui-scroll-wrapper').scroll({
+    indicators: false
+});
+
+mui('.mui-slider').slider({
+    interval:1000
+});
